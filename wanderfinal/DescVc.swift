e7a5d8cf32e7bbe5062c98add7ecf91571772232
MacStudent <macstudent@126-iMacXX-M.local>
2020-01-30 11:02:05 -0500

    basic ui

---
+++ wanderfinal/DescVc.swift
@@ -0,0 +1,38 @@
+//
+//  DescVc.swift
+//  wanderfinal
+//
+//  Created by MacStudent on 2020-01-30.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class DescVc: UIViewController {
+
+  
+    
+    
+    @IBOutlet weak var UserName: UITextField!
+    @IBOutlet weak var Birthday: UILabel!
+    @IBOutlet weak var Gender: UILabel!
+    @IBOutlet weak var UserImage: UIImageView!
+    @IBOutlet weak var Latitude: UITextField!
+    @IBOutlet weak var Longitude: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+    
+    @IBAction func Save(_ sender: Any) {
+   
+    
+    print("saved")
+    
+    }
+    
+}
